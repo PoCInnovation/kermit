4fe0e04e4e4873f0cfe34abede1160eb2b61fd55
Nathan FLATTIN <nathan.flattin@epitech.eu>
2025-10-21 11:08:28 +0900

    fix: network error

--- src/network/health.rs
+++ src/network/health.rs
@@ -3,18 +3,17 @@ use serde_json::Value;
 
 use crate::common::get;
 
-async fn is_network_alive(url: &str) -> Result<bool> {
-    let res = get::<Value>(url, "/infos/version").await?;
-    Ok(res.is_some())
+async fn is_network_alive(url: &str) -> bool {
+    get::<Value>(url, "/infos/version").await.is_ok()
 }
 
-/* See if the network is available before continuing */
+// See if the network is available before continuing
 pub async fn check_network(url: &str) -> Result<()> {
     if reqwest::Url::parse(url).is_err() {
         bail!("Invalid node URL: {url}");
     }
 
-    if !is_network_alive(url).await? {
+    if !is_network_alive(url).await {
         bail!("Network is not reachable: {url}");
     }
 
